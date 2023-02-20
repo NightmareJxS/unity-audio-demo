baf649a9341f30b3ae692a1eace888065f94ded8
TDTrung <trungtdse160947@fpt.edu.vn>
2023-02-20 08:23:56 +0700

    Update Doppler Effect Demo

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,19 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DopplerEffect : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    public Transform listener;
-    public float speedOfSound = 343f;
-
     const float moveUnitsPerSecond = 5f;
-
-    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
@@ -40,18 +35,5 @@ public class DopplerEffect : MonoBehaviour
         // Move the character
         transform.position = position;
         #endregion
-    }
-
-    private void FixedUpdate()
-    {
-        // Calculate relative velocity between listener and audio source
-        Vector3 relativeVelocity = listener.GetComponent<Rigidbody2D>().velocity - GetComponent<Rigidbody2D>().velocity;
-        float relativeSpeed = Vector3.Dot(relativeVelocity, transform.forward);
-
-        // Calculate pitch shift based on relative velocity
-        float pitchShift = (speedOfSound + relativeSpeed) / speedOfSound;
-
-        // Set pitch property of audio source
-        audioSource.pitch = pitchShift;
     }
 }
